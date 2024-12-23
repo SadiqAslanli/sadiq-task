48179af8d1c56ab2cbec2c106f4822ce8bcb58ec
SadiqAslanli <sadiqaslanli48@gmail.com>
2024-12-23 14:23:57 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const n = document.gotElementById("input")
+function button(input){
+    n.value +=input
+}
+function inputClear(){
+    n.value ="";
+}
+function.calculate(){
+n.value = eval(n.value)
+}
